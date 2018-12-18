8a36eeaee35f19308a0ba36268aa352ed12c769a
Bartosz Wasilik <bartoszwasilik12@gmail.com>
2018-12-18 17:37:36 +0100

    dodano despacito

--- app/static/jsScripts/alexa.js
+++ app/static/jsScripts/alexa.js
@@ -1,10 +1,14 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 let alexa = new SpeechRecognition()
 let  przycisk = document.querySelector('Input')
+const ss = window.speechSynthesis;
+let ssu = new SpeechSynthesisUtterance;
+let glosy = [];
 
-function sluchaj(e){
+function sluchaj(event){
     alert('Przekarze to')
-    console.dir(e.results[0][0].transcript)
+    console.dir(event.results[0][0].transcript)
+    alexaJobs(event.results[0][0].transcript)
 }
 
 alexa.addEventListener('result', sluchaj);
@@ -15,3 +19,31 @@ przycisk.addEventListener('click', function(e){
     alert('Dzwonie po prawnika')
     alexa.start()
 })
+
+alexa.addEventListener('end', ()=>{
+    console.log('alexa end')
+})
+
+ss.addEventListener('voiceschanged', ()=>{
+    glosy = ss.getVoices();
+    console.dir(glosy);
+})
+
+function alexaJobs(text){
+    text = text.toLowerCase()
+     // fnukcja includes zwraca true albo false
+
+//Polski
+     if(text.includes("witaj")){
+         //tu coś będzie
+         ssu.text = "Witaj przyjacielu"
+         ssu.lang = glosy[0];
+         ss.speak(ssu)
+     }
+
+//Angielski
+
+     else if(text.includes("hello")){
+        window.open('https://www.youtube.com/watch?v=kJQP7kiw5Fk', '_blank');
+    }
+}
